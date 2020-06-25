87bc89c0a1bbab5b39054d0d0cff6de6fb814b66
Pip Bbaale <namugera.philip@gmail.com>
2020-06-25 11:06:01 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoryComponent } from './components/category/category.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AddPostComponent } from './components/add-post/add-post.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomepageComponent},
+  { path: '', redirectTo:"/home", pathMatch:"full"},
+  { path: 'category', component: CategoryComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'profile', component: ProfileComponent},
+  { path: 'add_post', component: AddPostComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { AddPostComponent } from './components/add-post/add-post.component';
+import { CategoryComponent } from './components/category/category.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    AddPostComponent,
+    CategoryComponent,
+    HomepageComponent,
+    LoginComponent,
+    NavbarComponent,
+    ProfileComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
