24295c630549ba5a279852f3dc89c00a75c483a3
Pip Bbaale <namugera.philip@gmail.com>
2020-06-26 09:25:32 +0300

    getting post service

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -17,13 +17,14 @@ export class PostService {
   postUrl:string = 'https://techlandjarvis.herokuapp.com/posts/api/posts/'
 
   constructor(private http:HttpClient) { 
-
   }
   getPosts():Observable<Post[]> {
     return this.http.get<Post[]>(`${this.postUrl}`);
+
   }
 
   addPost(post:Post):Observable<Post> {
     return this.http.post<Post>(this.postUrl, post, httpOptions);
   }
+
 }
